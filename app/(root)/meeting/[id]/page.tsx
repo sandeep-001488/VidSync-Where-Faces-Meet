7968d2be02327b21c176eab527cf0655491f03a5
sandeep <sandeepsanu1230@gmail.com>
2025-01-15 00:54:10 +0530

    updated meeting again

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,45 +1,3 @@
-// "use client";
-
-// import Loader from "@/components/ui/Loader";
-// import MeetingRoom from "@/components/ui/MeetingRoom";
-// import MeetingSetup from "@/components/ui/MeetingSetup";
-// import { useGetCallById } from "@/hooks/useGetCallById";
-// import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-// import { useParams } from "next/navigation";
-// import React, { useState } from "react";
-// import ErrorBoundary from "@/components/ui/ErrorBoundary";
-
-// const Meeting = () => {
-//   const [isSetupComplete, setIsSetupComplete] = useState(false);
-//   const params = useParams();
-//   const id = params?.id as string | undefined; 
-
-//   const { call, isCallLoading } = useGetCallById(id || "");
-
-//   if (!id || !call || isCallLoading) {
-//     return <Loader />;
-//   }
-
-      
-
-//   return (
-//     <ErrorBoundary>
-//       <main className="text-white h-screen w-full">
-//         <StreamCall call={call}>
-//           <StreamTheme>
-//             {!isSetupComplete ? (
-//               <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-//             ) : (
-//               <MeetingRoom />
-//             )}
-//           </StreamTheme>
-//         </StreamCall>
-//       </main>
-//     </ErrorBoundary>
-//   );
-// };
-
-// export default Meeting;
 "use client";
 
 import Loader from "@/components/ui/Loader";
@@ -47,22 +5,21 @@ import MeetingRoom from "@/components/ui/MeetingRoom";
 import MeetingSetup from "@/components/ui/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-import { useRouter } from "next/router";
+import { useParams } from "next/navigation";
 import React, { useState } from "react";
 import ErrorBoundary from "@/components/ui/ErrorBoundary";
 
-
-
 const Meeting = () => {
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const router = useRouter();
-  const { id } = router.query;
+  const params = useParams();
+  const id = params?.id as string | undefined; 
 
-  const { call, isCallLoading } = useGetCallById(id as string);
+  const { call, isCallLoading } = useGetCallById(id || "");
 
   if (!id || !call || isCallLoading) {
     return <Loader />;
   }
+
 
   return (
     <ErrorBoundary>
