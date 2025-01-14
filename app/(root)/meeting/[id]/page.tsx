fe86bc147341115217f39cb8a04de9d23721983c
sandeep <sandeepsanu1230@gmail.com>
2025-01-14 22:59:19 +0530

    updated meeting page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,37 +1,3 @@
-// "use client";
-
-// import Loader from "@/components/ui/Loader";
-// import MeetingRoom from "@/components/ui/MeetingRoom";
-// import MeetingSetup from "@/components/ui/MeetingSetup";
-// import { useGetCallById } from "@/hooks/useGetCallById";
-// import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-// import React, { useState } from "react";
-
-// const Meeting = ({ params }: { params: Promise<{ id: string }> }) => {
-//   // const { user, isLoaded } = useUser();
-//   const [isSetupComplete, setIsSetupComplete] = useState(false);
-
-//   const { id } = React.use(params);
-//   const { call, isCallLoading } = useGetCallById(id);
-
-//   if (!call || isCallLoading) return <Loader />;
-
-//   return (
-//     <main className="text-white h-screen w-full">
-//       <StreamCall call={call}>
-//         <StreamTheme>
-//           {!isSetupComplete ? (
-//             <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-//           ) : (
-//             <MeetingRoom />
-//           )}
-//         </StreamTheme>
-//       </StreamCall>
-//     </main>
-//   );
-// };
-
-// export default Meeting;
 "use client";
 
 import Loader from "@/components/ui/Loader";
@@ -39,19 +5,19 @@ import MeetingRoom from "@/components/ui/MeetingRoom";
 import MeetingSetup from "@/components/ui/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
 import React, { useState } from "react";
 
-interface MeetingProps {
-  params: {
-    id: string;
-  };
-}
-
-const Meeting = ({ params }: MeetingProps) => {
+const Meeting = () => {
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const { call, isCallLoading } = useGetCallById(params.id);
+  const params = useParams();
+  const id = params?.id as string | undefined; 
 
-  if (!call || isCallLoading) return <Loader />;
+  const { call, isCallLoading } = useGetCallById(id || "");
+
+  if (!id || !call || isCallLoading) {
+    return <Loader />;
+  }
 
   return (
     <main className="text-white h-screen w-full">
